2e2aa1fe109f2084a5218c8978ec6f6eee669cad
emilbankov <emilbankov17d@gmail.com>
2025-06-01 20:36:59 +0300

    Created tinting page

---
+++ src/components/TintingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CarModel from '../components/CarModel';
+
+const TintingPage = () => {
+  return (
+    <div className="tinting-page">
+      <CarModel />
+    </div>
+  );
+};
+
+export default TintingPage;
