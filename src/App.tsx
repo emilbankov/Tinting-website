7065d6b07e5dda236ebcc1a333e93d1bb2ad3566
emilbankov <emilbankov17d@gmail.com>
2025-06-01 20:34:29 +0300

    Initialize vite react project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TintingPage from './components/TintingPage'
+import './App.css'
+const App = () => {
+  return (
+    <div className="App">
+      <TintingPage />
+    </div>
+  )
+}
+
+export default App
