5da063e30d02af118a71c30d7cfd8823b811a59a
emilbankov <emilbankov17d@gmail.com>
2026-02-19 00:51:04 +0200

    Dynamic header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,180 +1,52 @@
-export default function HomeHeader() {
+import { Link, useLocation } from "react-router-dom"
+
+export default function Header() {
+    const location = useLocation()
+
+    // Dynamic page info based on route
+    const getPageInfo = () => {
+        const path = location.pathname
+        
+        if (path === '/about-us') {
+            return { title: 'За нас', breadcrumb: 'За нас' }
+        } else if (path === '/tinting') {
+            return { title: 'Тониране', breadcrumb: 'Тониране' }
+        } else {
+            return { title: 'Начало', breadcrumb: null }
+        }
+    }
+
+    const pageInfo = getPageInfo()
+
     return (
         <>
             <header>
                 <div className="header-top">
-                    <div className="currency">
-                        <span>Валута:</span>
-                        <div className="currency-choice clearfloat">
-                            GBP <i className="fa-regular fa-angle-down" />
-                        </div>
-                        <ul className="currency-list">
-                            <li>
-                                <a href="" onclick="choiceCurrency(2); return false;">
-                                    EUR
-                                </a>
-                            </li>
-                            <li>
-                                <a href="" onclick="choiceCurrency(5); return false;">
-                                    USD
-                                </a>
-                            </li>
-                            <li>
-                                <a href="" onclick="choiceCurrency(6); return false;">
-                                    GBP
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="languages">
-                        <span>Избери език:</span>
-                        <div className="lang-choice clearfloat">
-                            <img src="templates/default/images/flag_bg.png" alt="bg" />
-                            <i className="fa-regular fa-angle-down" />
-                        </div>
-                        <ul className="lang-list">
-                            <li>
-                                <a
-                                    href="bg/Za_nas-c230p"
-                                    onclick="choiceCurrency(1); return true;"
-                                    hrefLang="Български"
-                                >
-                                    <img src="templates/default/images/flag_bg.png" alt="bg" />
-                                    Български
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="en/About_Us-c230"
-                                    onclick="choiceCurrency(6); return true;"
-                                    hrefLang="English"
-                                >
-                                    <img src="templates/default/images/flag_en.png" alt="en" />
-                                    English
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="de/_ber_uns-c230"
-                                    onclick="choiceCurrency(2); return true;"
-                                    hrefLang="Deutsch"
-                                >
-                                    <img src="templates/default/images/flag_de.png" alt="de" />
-                                    Deutsch
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="fr/A_propos_de-c230"
-                                    onclick="choiceCurrency(2); return true;"
-                                    hrefLang="Francais"
-                                >
-                                    <img src="templates/default/images/flag_fr.png" alt="fr" />
-                                    Francais
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="se/Om_oss-c230"
-                                    onclick="choiceCurrency(2); return true;"
-                                    hrefLang="Svenska"
-                                >
-                                    <img src="templates/default/images/flag_se.png" alt="se" />
-                                    Svenska
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="it/Chi_siamo-c230"
-                                    onclick="choiceCurrency(2); return true;"
-                                    hrefLang="Italiano"
-                                >
-                                    <img src="templates/default/images/flag_it.png" alt="it" />
-                                    Italiano
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="gr/_-c230"
-                                    onclick="choiceCurrency(2); return true;"
-                                    hrefLang="Ελληνικά"
-                                >
-                                    <img src="templates/default/images/flag_gr.png" alt="gr" />
-                                    Ελληνικά
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="pl/O_nas-c230"
-                                    onclick="choiceCurrency(2); return true;"
-                                    hrefLang="Polski"
-                                >
-                                    <img src="templates/default/images/flag_pl.png" alt="pl" />
-                                    Polski
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="Qui-nes-somos-11-223"
-                                    onclick="choiceCurrency(2); return true;"
-                                    hrefLang="Español"
-                                >
-                                    <img src="templates/default/images/flag_es.png" alt="es" />
-                                    Español
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
                     <div className="header-top-right">
-                        <div className="search-form">
-                            <div className="search-wrap clearfloat">
-                                <form className="inline" autoComplete="off">
-                                    <input
-                                        type="text"
-                                        id="searchForm"
-                                        className="search-input"
-                                        placeholder="Търсене"
-                                        autoComplete="off"
-                                    />
-                                </form>
-                                <a
-                                    href=""
-                                    className="search-submit"
-                                    onclick="searchForKeyword();
-                                 return false;"
-                                    title="Търсене"
-                                >
-                                    <i className="fa-regular fa-search" />
-                                </a>
-                                <div className="fluid-menu" style={{ display: "none" }}>
-                                    <h4>Моля, напишете минимум 3 символа</h4>
-                                </div>
-                            </div>
-                        </div>
                         <div className="header-contacts">
                             <a href="tel:+359898841010" title="+359 898 84 10 10">
-                                <i className="fa-brands fa-viber" />
-                                <span>+359 898 84 10 10</span>
+                                <i className="fa-solid fa-phone" />
+                                <span>+359 898 142 612</span>
                             </a>
-                            <a href="mailto:office@tintcom.com" title="office@tintcom.com">
-                                <i className="fa-regular fa-envelopes" />
-                                <span>office@tintcom.com</span>
+                            <a href="mailto:stoqnegay@tintify.com" title="stoqnegay@tintify.com">
+                                <i className="fa-regular fa-envelope" />
+                                <span>stoqnegay@tintify.com</span>
                             </a>
                         </div>
                     </div>
                 </div>
                 <div className="header-main">
                     <div className="logo">
-                        <a href="/">
-                            <img src="templates/default/images/logo.png" alt="Tintcom" />
-                        </a>
+                        <Link to="/">
+                            <img src="/images/logo.png" alt="Tintcom" />
+                        </Link>
                     </div>
                     <nav id="menu">
                         <ul>
                             <li className="lvl-1">
-                                <a className="ml-223" href="bg/Za_nas-c230p">
+                                <Link className="ml-223" to="/about-us">
                                     За нас
-                                </a>
+                                </Link>
                             </li>
                         </ul>
                     </nav>
@@ -245,11 +117,13 @@ export default function HomeHeader() {
                         <div className="cart">
                             <a
                                 href="/page/step1"
-                                onclick="$(this).closest('.cart').toggleClass('active');
-                         return false;"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    document.querySelector('.cart').classList.toggle('active');
+                                }}
                                 title="Потребителска кошница"
                             >
-                                <i className="fa-regular fa-shopping-cart" />
+                                <i className="fa-solid fa-cart-shopping" />
                                 <span className="broi-number">0</span>
                                 <em>0.00 £</em>
                             </a>
@@ -264,15 +138,20 @@ export default function HomeHeader() {
             <div className="ban-inner">
                 <div className="ban-inner-text">
                     <div className="wrapper clearfloat">
-                        <h1>За нас</h1>
+                        <h1>{pageInfo.title}</h1>
                         <div className="breadcrumb">
-                            <a href="/">Начало</a>
-                            <a href="bg/Za_nas-c230p">За нас</a>
+                            <Link to="/">Начало</Link>
+                            {pageInfo.breadcrumb && (
+                                <>
+                                    <span> / </span>
+                                    <Link to={location.pathname}>{pageInfo.breadcrumb}</Link>
+                                </>
+                            )}
                         </div>
                     </div>
                 </div>
                 <div className="ban-inner-img">
-                    <img src="../files/default/bnr.jpg" alt="default" />
+                    <img src="/images/bnr.jpg" alt="default" />
                 </div>
             </div>
         </>
