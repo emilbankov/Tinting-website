045e0a5f552d810b426e6a62ec5ff1ab025d9437
emilbankov <emilbankov17d@gmail.com>
2026-02-19 00:12:36 +0200

    Added about page

---
+++ src/components/About Us/AboutUs.tsx
@@ -0,0 +1,146 @@
+export default function Home() {
+    return (
+        <>
+            <div className="content">
+                <div className="wrapper clearfloat">
+                    <div className="text-module clearfloat">
+                        ТИНТКОМ ООД е фирма занимаваща се с внос и продажба на
+                        висококачествените американски авто фолиа на фирмите- Solar Gard и
+                        Johnson Window Films.
+                        <br />
+                        <br />
+                        Ние можем да изрежем фолио за тониране (затъмняване) на автомобилни
+                        стъкла за над 90% от всички марки и модели автомобили в света за:
+                        <ul>
+                            <li>
+                                Предни странични прозорци (плюс малките прозорци, ако автомобила
+                                разполага с такива)
+                            </li>
+                            <li>
+                                Задни странични прозорци (плюс малките прозорци, ако автомобила
+                                разполага с такива)
+                            </li>
+                            <li>Задни прозорци</li>
+                            <li>Сенници</li>
+                        </ul>
+                        <br />
+                        <br />
+                    </div>
+                    <div className="text-module clearfloat">
+                        <article className="one-new clearfloat">
+                            <div className="one-new-info">
+                                <h5 className="one-new-title">
+                                    <a href="bg/Solar_Gard_seriya_NR_Smoke_Plus_classic_smoke_grey_-p6548">
+                                        Solar Gard - серия NR Smoke Plus (classic smoke grey)
+                                    </a>
+                                </h5>
+                                <div className="text">
+                                    Серията на&nbsp;Solar&nbsp;Gard - NR Smoke&nbsp;Plus&nbsp;/classic
+                                    smoke grey/&nbsp;е&nbsp;фолио за тониране (затъмняване) на
+                                    автостъкла,&nbsp;което&nbsp;ви предлага класическа визия на
+                                    страхотна цена.&nbsp;Серията NR&nbsp;Smoke&nbsp;Plus&nbsp;не пречи
+                                    на&nbsp;GPS,&nbsp;сателитното радио и обхвата на клетъчния
+                                    телефон.&nbsp;Серията NR&nbsp;Smoke
+                                    Plus&nbsp;предлага&nbsp;UV&nbsp;защита, така ще предпазите
+                                    интериора си от избледняване&nbsp;и ще осигурите по-добра работа
+                                    на вашия климатик&nbsp;през горещите дни.&nbsp;Издръжливост на
+                                    цвета 5 години.
+                                </div>
+                                <a
+                                    href="bg/Solar_Gard_seriya_NR_Smoke_Plus_classic_smoke_grey_-p6548"
+                                    className="btn"
+                                >
+                                    Детайли
+                                </a>
+                            </div>
+                        </article>
+                        <article className="one-new clearfloat">
+                            <div className="one-new-info">
+                                <h5 className="one-new-title">
+                                    <a href="bg/Solar_Gard_Supreme_Performance_Series-p6549">
+                                        Solar Gard® Supreme: Performance Series
+                                    </a>
+                                </h5>
+                                <div className="text">
+                                    Следвайки традицията на &nbsp;Solar Gard &nbsp;за най-високо
+                                    качество на най-добри цени, новата &nbsp;Supreme &nbsp;серия
+                                    подобрява комфорта ви по време на шофиране, може да изберете един
+                                    от петте нюанса &nbsp;, за да подобрите вида на колата
+                                    си.&nbsp;Блокира повече от 99% от ултравиолетовите лъчи &nbsp;и
+                                    &nbsp;ще ви бъде осигурена максимална защита от тях.&nbsp;В
+                                    допълнение, ще бъдете щастливи да научите, че &nbsp;Supreme
+                                    &nbsp;серията &nbsp;е водеща в индустрията, в раздела за яснота на
+                                    фолиото,&nbsp;което &nbsp;допринася за по безопасното шофиране. 10
+                                    години издръжливост на цвета.
+                                </div>
+                                <a
+                                    href="bg/Solar_Gard_Supreme_Performance_Series-p6549"
+                                    className="btn"
+                                >
+                                    Детайли
+                                </a>
+                            </div>
+                        </article>
+                        <article className="one-new clearfloat">
+                            <div className="one-new-info">
+                                <h5 className="one-new-title">
+                                    <a href="bg/Jonson_Window_Films_seriya_Ray_Guard_Charcoal-p6550">
+                                        Jonson Window Films - серия Ray Guard Charcoal
+                                    </a>
+                                </h5>
+                                <div className="text">
+                                    Независимо дали това е първия ви спортен автомобил,
+                                    мултифункционален пикап или семейният ви ван,&nbsp;Ray Guard
+                                    &nbsp;на &nbsp;Johnson Window Films &nbsp;ви предоставя двоен
+                                    пакет от страхотен външен вид и огромна полезност. Серията
+                                    &nbsp;Ray Guard абсорбира светлината и &nbsp;предлага ефективна
+                                    соларна/ слънчева и &nbsp;UV &nbsp;защита, осигурява защитен слой
+                                    за интериора на вашия автомобил.&nbsp;Много от днешните автомобили
+                                    са снабдени с антена на задното стъкло,&nbsp;GPS &nbsp;и сателитни
+                                    системи, структурата на фолиото за затъмняване (тониране) на
+                                    автостъкла -&nbsp;Ray Guard &nbsp;е непоглъщаща и следователно не
+                                    пречи на този тип електронни системи.&nbsp;Също така серията
+                                    &nbsp;Ray Guard &nbsp;идва в 5 нюанса черен въгленов оттенък, от
+                                    &nbsp;които вие можете да направите вашия избор. Ray Guard
+                                    &nbsp;ще предаде на вашия автомобил страхотна визия и стил.
+                                </div>
+                                <a
+                                    href="bg/Jonson_Window_Films_seriya_Ray_Guard_Charcoal-p6550"
+                                    className="btn"
+                                >
+                                    Детайли
+                                </a>
+                            </div>
+                        </article>
+                        <article className="one-new clearfloat">
+                            <div className="one-new-info">
+                                <h5 className="one-new-title">
+                                    <a href="bg/Johnson_Window_Films_seriya_Marathon_Universal_Black-p6551">
+                                        Johnson Window Films - серия Marathon Universal Black
+                                    </a>
+                                </h5>
+                                <div className="text">
+                                    Първокласен цвят, първокласно представяне и първокласна
+                                    издръжливост. Това получавате, когато изберете фолио за
+                                    &nbsp;затъмняване (тониране) на автостъкла &nbsp;Marathon
+                                    &nbsp;„universal black "на компания &nbsp;Johnson Window
+                                    Films.&nbsp;Сериите на &nbsp;Marathon &nbsp;са постигнали
+                                    идеалната комбинация между визия и соларно изпълнение, за да
+                                    можете много години да се наслаждавате на удоволствието от
+                                    шофирането.
+                                </div>
+                                <a
+                                    href="bg/Johnson_Window_Films_seriya_Marathon_Universal_Black-p6551"
+                                    className="btn"
+                                >
+                                    Детайли
+                                </a>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </>
+
+    );
+};
