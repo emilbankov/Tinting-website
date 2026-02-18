e04a7df192171fcd36a93a457a3711c77441253c
emilbankov <emilbankov17d@gmail.com>
2026-02-19 00:12:47 +0200

    Add header, back-to-top, mobile menu, and footer
    Import and render additional layout components: HomeHeader, BackToTop, MobileMenu, and Footer. Re-enable Home and comment out TintingPage, updating component order in App.tsx to include the new header and site chrome.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import Home from './components/Home/Home'
+import HomeHeader from './components/Home Header/HomeHeader'
 import TintingPage from './components/TintingPage'
+import BackToTop from './components/Back To Top/BackToTop'
+import MobileMenu from './components/Mobile Menu/MobileMenu'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
     <div className="App">
-      {/* <Home /> */}
-      <TintingPage />
+      <HomeHeader />
+      <Home />
+      {/* <TintingPage /> */}
+      <BackToTop />
+      <MobileMenu />
+      <Footer />
     </div>
   )
 }
