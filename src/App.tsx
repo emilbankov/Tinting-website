2847cf6b91114f8c287448f7f499c1e0d5701dbe
emilbankov <emilbankov17d@gmail.com>
2026-02-19 00:50:33 +0200

    Installed react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
+import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home/Home'
 import HomeHeader from './components/Home Header/HomeHeader'
 import TintingPage from './components/TintingPage'
 import BackToTop from './components/Back To Top/BackToTop'
 import MobileMenu from './components/Mobile Menu/MobileMenu'
 import Footer from './components/Footer/Footer'
+import AboutUs from './components/About Us/AboutUs'
+import Header from './components/Header/Header'
+import { useLocation } from 'react-router-dom'
 
 const App = () => {
-  return (
-    <div className="App">
-      <HomeHeader />
-      <Home />
-      {/* <TintingPage /> */}
-      <BackToTop />
-      <MobileMenu />
-      <Footer />
-    </div>
-  )
+    const location = useLocation()
+    const isHomePage = location.pathname === '/'
+
+    return (
+        <div className="App">
+            {isHomePage ? <HomeHeader /> : <Header />}
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about-us" element={<AboutUs />} />
+                <Route path="/tinting" element={<TintingPage />} />
+            </Routes>
+
+            <BackToTop />
+            <MobileMenu />
+            <Footer />
+        </div>
+    )
 }
 
 export default App
